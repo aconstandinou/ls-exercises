7aeb679e6379b11b9ac0528816e4a5d8da4d1f51
antoniozeus <constandinou.antonio@gmail.com>
2017-04-22 14:44:47 -0500

    exercises

---
+++ 101_109_small_problems/assessment_review/programming_foundations_problem.rb
@@ -0,0 +1,51 @@
+require 'pry'
+
+order_data = [
+  {customer_id: 12, customer_name: 'Emma Lopez', order_id: 351, order_date: '12/04/16', order_fulfilled: true, order_value: 135.99},
+  {customer_id: 12, customer_name: 'Emma Lopez', order_id: 383, order_date: '12/04/16', order_fulfilled: true, order_value: 289.49},
+  {customer_id: 12, customer_name: 'Emma Lopez', order_id: 392, order_date: '01/10/17', order_fulfilled: false, order_value: 58.00},
+  {customer_id: 32, customer_name: 'Michael Richards', order_id: 241, order_date: '11/10/16', order_fulfilled: true, order_value: 120.00},
+  {customer_id: 32, customer_name: 'Michael Richards', order_id: 395, order_date: '01/10/17', order_fulfilled: false, order_value: 85.65},
+]
+
+customer_orders = {}
+
+order_data.each do |row|
+  if customer_orders.has_key?(row[:customer_id])
+    customer_orders[row[:customer_id]][:orders] << {
+      order_fulfilled: row[:order_fulfilled],
+      order_value: row[:order_value]
+    }
+  else
+    customer_orders[row[:customer_id]] = {
+      customer_id: row[:customer_id],
+      customer_name: row[:customer_name],
+      orders: [
+        {
+          order_fulfilled: row[:order_fulfilled],
+          order_value: row[:order_value]
+        }
+      ]
+    }
+  end
+end
+
+customer_orders = customer_orders.values
+
+
+
+fulfilled_orders = customer_orders.map do |customer|
+  {customer_id: customer[:customer_id],
+   customer_name: customer[:customer_name]}
+end
+
+customer_orders.each_with_index do |data, index|
+  order_value = data[:orders].reduce(0) do |total, order|
+    binding.pry
+    total + order[:order_value] if order[:order_fulfilled]
+  end
+
+  fulfilled_orders[index][:order_value] = order_value
+end
+
+puts fulfilled_orders
---
+++ 101_109_small_problems/assessment_review/raw_ruby_code.rb
@@ -0,0 +1,2 @@
+#method input: two integers as arguments
+#method output: return their product
