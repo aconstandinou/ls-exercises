f7a2461d131c8d7d902cd8350c2ec675468c548a
antoniozeus <constandinou.antonio@gmail.com>
2017-04-29 13:30:31 -0500

    exercises

--- 101_109_small_problems/assessment_review/raw_ruby_code.rb
+++ 101_109_small_problems/assessment_review/raw_ruby_code.rb
@@ -33,3 +33,43 @@ p featured(997) == 1029
 p featured(1029) == 1043
 p featured(999_999) == 1_023_547
 p featured(999_999_987) == 1_023_456_987
+
+# balancer problem
+
+def balancer(string)
+  return false unless string.count('(') == string.count(')')
+
+  paren_count = 0
+  string.split('').each do |char|
+    paren_count += 1 if char == '('
+    paren_count -= 1 if char == ')'
+    return false if paren_count < 0
+  end
+  true
+end
+
+p balancer('()()()()')
+p balancer('()()()))')
+p balancer('he()ll(')
+
+# remove all vowels from string, then remove all non-alphanumeric chars from array of strings
+
+def remove_vowels(string)
+  array_string = string.split('')
+  array_string.each do |char|
+    char.gsub!(/[aeiou]/i, "")
+  end
+
+  array_string.join
+end
+
+def remove_nonalpha(array_string)
+  array_string.each do |word|
+    word.gsub!(/[^a-zA-Z0-9]/i, "")
+  end
+
+  array_string
+end
+
+remove_vowels("hello world, how are you doing today?")
+remove_nonalpha("he!!o wor!d, how? @re you do#ng tod@y 1999?".split(' '))
