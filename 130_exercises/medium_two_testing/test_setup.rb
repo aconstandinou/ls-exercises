98d2157773c857c8281db692c4ee539934b26549
antoniozeus <constandinou.antonio@gmail.com>
2017-08-10 22:50:12 -0500

    exercises for course 130

--- 130_exercises/medium_two_testing/test_setup.rb
+++ 130_exercises/medium_two_testing/test_setup.rb
@@ -57,4 +57,12 @@ class CashRegisterTest < MiniTest::Test
     transaction.prompt_for_payment(input: input)
     assert_equal 30, transaction.amount_paid
   end
+
+  def test_prompt_payment_V3
+    transaction = Transaction.new(30)
+    input = StringIO.new('30\n')
+    output = StringIO.new #mock object for output
+    transaction.prompt_for_payment(input: input, output: output)
+    assert_equal 30, transaction.amount_paid
+  end
 end
