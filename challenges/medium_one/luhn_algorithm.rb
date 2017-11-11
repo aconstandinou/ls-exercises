cffb4f4e834e742dcceeb78f7fc20b11a580707a
antoniozeus <constandinou.antonio@gmail.com>
2017-11-11 14:33:37 -0600

    LS Challenge Problems

--- challenges/medium_one/luhn_algorithm.rb
+++ challenges/medium_one/luhn_algorithm.rb
@@ -2,24 +2,15 @@ class Luhn
 
   AVAIL_DIGITS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-  attr_reader :whole_num, :whole_num_split
-
   def initialize(number)
     @whole_num = number
-    @whole_num_split = self.whole_num.to_s.split('').reverse.map { |dgt| dgt.to_i }
+    @whole_num_split = @whole_num.to_s.split('').reverse.map { |dgt| dgt.to_i }
   end
 
   def addends
-    addends_array = self.whole_num_split.map.with_index do |val, idx|
+    addends_array = @whole_num_split.map.with_index do |val, idx|
       if idx.odd?
-        val * 2
-      else
-        val
-      end
-    end
-    addends_array = addends_array.map do |val|
-      if val >= 10
-        val - 9
+        val * 2 > 10 ? val * 2 - 9 : val * 2
       else
         val
       end
@@ -28,7 +19,7 @@ class Luhn
   end
 
   def checksum
-    addends.reduce(:+)
+    addends.reduce(&:+)
   end
 
   def valid?
---
+++ challenges/medium_one/pig_latin.rb
@@ -0,0 +1,57 @@
+# Rule 1: If word begins with vowel sound, add an "ay" sound to end of the word.
+# Rule 2: If a word begins with a consonant, move it to the end of the word,
+# and then add an "ay" sound to the end of the word.
+
+# input: string (can be multiple words)
+# output: new string where each word (if multiple) is translated
+# calling method: class method translate
+
+class PigLatin
+
+  VOWELS = ['a', 'e', 'i', 'o', 'u']
+  VOWELS_SPECIAL = ['qu', 'x']
+
+  def self.translate(string)
+    string_arr = string.split(' ')
+    pig_latin_arr = []
+
+    string_arr.each do |word|
+      if VOWELS.include?(word[0])
+        pig_latin_arr << word + 'ay'
+      elsif word[0] == 'y'
+        if VOWELS.include?(word[1])
+          pig_latin_arr << word[1..-1] + 'yay'
+        else
+          pig_latin_arr << word + 'ay'
+        end
+      elsif word.include?('qu')
+        index_at = word.index('u') + 1
+        pig_latin_arr << word[index_at..-1] + word[0..index_at-1] + 'ay'
+      elsif word[0] == 'x'
+        if VOWELS.include?(word[1])
+          pig_latin_arr << word[1..-1] + 'xay'
+        else
+          pig_latin_arr << word + 'ay'
+        end
+      else
+        first_letters = ''
+        idx = 0
+        char_split = word.split('')
+
+        char_split.each_with_index do |char, index|
+          if VOWELS.include?(char)
+            idx = index
+            break
+          else
+            first_letters << char
+          end
+        end
+        keep = word.slice(idx..-1)
+        pig_latin_arr << keep + first_letters + 'ay'
+      end
+    end
+
+    pig_latin_arr.join(' ')
+  end
+
+end
